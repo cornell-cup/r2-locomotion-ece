84ba33729f09a52a6069bbabb05c1626d74d0130
shaneelu <sl2363@cornell.edu>
2019-04-28 16:08:37 -0400

    Update precisearm.py

--- HeadLocomotion/precisearm.py
+++ HeadLocomotion/precisearm.py
@@ -5,10 +5,10 @@ ser_precise = 0
 wave_commands = ["M2D90E", "M3D60E", "M5D90E", "M3D90E", "M3D30E", "M3D90E", "M3D30E", "M3D90E", "M3D30E", "M0D0E"]
 grab_commands = ["M1D90E", "M2D90E", "M3D60E", "M5D-90E", "M6D45E", "M6D0E", "M0D0E"]
 
-def open():
+def open_ser_precise():
     ser_precise = serial.Serial('/dev/precisearm', 9600)
     
-def reset():
+def reset_precise():
     ser_precise.write("M0D0E".encode('utf-8'))
     ser_precise.close()
     
