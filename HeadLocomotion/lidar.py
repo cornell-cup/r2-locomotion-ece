6d1f348f7d221b0693149a5f712d0309859923b3
shaneelu <sl2363@cornell.edu>
2019-04-28 10:15:36 -0400

    Update lidar.py

--- HeadLocomotion/lidar.py
+++ HeadLocomotion/lidar.py
@@ -11,20 +11,24 @@ PORT_NAME = '/dev/ttyUSB0'
 def run():
     '''Main function'''
     lidar = RPLidar(PORT_NAME)
-    outfile = open('lidarResults.txt', 'w')
+    #outfile = open('lidarResults.txt', 'w')
     try:
         print('Recording measurments... Press Crl+C to stop.')
         for measurment in lidar.iter_measurments():
             i = 0
+            print(measurment)
             for distance in measurment:
+                print(distance)
                 if distance == False and i != 0:
+                    print("in return")
                     return False
+                print(i)
                 i = i + 1
     except KeyboardInterrupt:
         print('Stopping.')
     lidar.stop()
     lidar.disconnect()
-    outfile.close()
+    #outfile.close()
 
 if __name__ == '__main__':
     run()
